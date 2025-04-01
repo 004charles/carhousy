ae82419159ec71f097859fcbec830c5fa4c7dcb0
NelsonMuquissi <emelsonmuquissi@gmail.com>
2025-04-01 05:23:55 +0100

    New database/postgresql

--- carhouse_projecto/settings.py
+++ carhouse_projecto/settings.py
@@ -71,19 +71,19 @@ WSGI_APPLICATION = 'carhouse_projecto.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-"""DATABASES = {
+DATABASES = {
     'default': dj_database_url.config(
         default=os.getenv('DATABASE_URL'),
         conn_max_age=600
     )
-}"""
-
+}
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}"""
 
 
 # Password validation
